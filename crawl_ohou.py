d8ba61674652885b33110289f96d74271c8f09b8
dongheemin <crydevil5@nate.com>
2020-12-08 13:36:36 +0900

    기능 점검

--- crawl_ohou.py
+++ crawl_ohou.py
@@ -19,7 +19,7 @@ def doScrollDown(whileSecond, driver):
             break
 
 def ohou_crawling(url, mode = 0):
-    #driver init
+    # driver init
     driver_path = "driver/chromedriver.exe"
     driver = webdriver.Chrome(executable_path=driver_path)
 
@@ -37,11 +37,12 @@ def ohou_crawling(url, mode = 0):
         print("부정확한 url입니다.")
         return -1
 
-    #이동 대기
+    # 이동 대기
     time.sleep(5.5)
 
     # 세부아이템 url 수집
     soup = BeautifulSoup(driver.page_source, "lxml")
+    reviews = {}
     # doScrollDown(3, driver) #스크롤 다운기능
     for links in soup.find_all("a",{'class':'production-item__overlay'}):
         if 'href' in links.attrs:
@@ -54,41 +55,37 @@ def ohou_crawling(url, mode = 0):
             driver.get(link)
             time.sleep(3.5)
 
-            #제목 수집
+            # 제목 수집
             html = driver.page_source
             soup = BeautifulSoup(html, "lxml")
             title = re.sub('(<([^>]+)>)','$',str(soup.find_all('span', {'class':'production-selling-header__title__name'})))
 
-            #리뷰 수집
-            while(1):
+            # 리뷰 수집
+            while true:
                 try:
                     html = driver.page_source
                     soup = BeautifulSoup(html, "lxml")
                     temp = re.sub('(<([^>]+)>)','$',str(soup.find_all('p', {'class':'production-review-item__description'})))
                     date = re.sub('(<([^>]+)>)','$',str(soup.find_all('span', {'class':'production-review-item__writer__info__date'})).replace("\n", ""))
                     for tem in temp.split('$'):
-                        if(tem != ', ' and tem != ']' and tem != '['):
+                        if tem != ', ' and tem != ']' and tem != '[':
                             review.append(tem.replace("\n", ""))
 
                     for dat in date.split('$'):
-                        if(dat != ', ' and dat != ']' and dat != '['):
+                        if dat != ', ' and dat != ']' and dat != '[':
                             dat = dat.split(' ∙ ')
                             dates.append(dat[0])
 
-                    driver.find_element_by_class_name('_2XI47._3I7ex').click() #다음버튼
-                    time.sleep(1) #로딩 대기
+                    driver.find_element_by_class_name('_2XI47._3I7ex').click() # 다음버튼 (마지막까지 가면 없어짐)
+                    time.sleep(1) # 로딩 대기
 
                 except:
+                    reviews[title] = review+dates
                     break
-    # print(len(review))
-    # print(len(dates))
-    # print(len(shoppings))
-    output = pd.DataFrame({
-        title: review,
-        '구매일':dates
-    })
+
+    output = pd.DataFrame(reviews)
     output.to_excel(excel_writer='./output_ohou.csv')
 
-#오늘의 집 링크 입력 => query="Product_Name"
-#mode = 0 (기본), mode = 1 (리뷰순), mode = 2 (판매순)
+# 오늘의 집 링크 입력 => query="Product_Name"
+# mode = 0 (기본), mode = 1 (리뷰순), mode = 2 (판매순)
 ohou_crawling("https://ohou.se/productions/feed?query=%EB%84%A4%EC%98%A4%ED%94%8C%EB%9E%A8", mode=1)
